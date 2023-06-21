c16dec3a76d684ad421491f9142e704f4a5736b9
Gabi <gabi.vega1988@gmail.com>
2023-06-21 11:23:16 -0300

    8

--- server/index.js
+++ server/index.js
@@ -113,6 +113,6 @@ io.on("connection", (socket, usuario) => {
 
   });
   
-  // httpServer.listen(PORT, ()=> {
-  //   console.log(`SOCKET_IO Server Running on port ${PORT}`)
-  // });
+  httpServer.listen(3002, ()=> {
+    console.log(`SOCKET_IO Server Running on port 3002`)
+  });
