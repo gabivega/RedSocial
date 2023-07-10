490e6e8d4ed475f0b895d7718d112891275a3d6a
Gabi <gabi.vega1988@gmail.com>
2023-07-10 11:07:39 -0300

    updates7

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -22,6 +22,7 @@ export const createPost = async (req, res) => {
     });
     await newPost.save();
     const post = await Post.find();
+    console.log(post);
     res.status(201).json(post);
   } catch (err) {
     res.status(409).json({ message: err.message });
