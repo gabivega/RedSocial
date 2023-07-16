52c60df2a63e87deaefbd7d124044d077994506c
Gabi <gabi.vega1988@gmail.com>
2023-07-16 11:38:43 -0300

    updates14

--- server/index.js
+++ server/index.js
@@ -30,7 +30,7 @@ app.use(morgan("common"));
 app.use(bodyParser.json( { limit: "30mb", extended : true }));
 app.use(bodyParser.urlencoded( { limit: "30mb", extended: true }));
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: '*',
   methods: ["GET", "POST", "DELETE"]
 }));
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
@@ -83,7 +83,7 @@ const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: {
     origin: '*',
-    methods: ["GET", "POST","DELETE"]
+    methods: ["GET", "POST"]
   }
 });
 
