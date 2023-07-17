93b632268a5b434767eee367958b9909d9e7d2e9
Gabi <gabi.vega1988@gmail.com>
2023-07-17 14:16:57 -0300

    update2

--- server/index.js
+++ server/index.js
@@ -82,7 +82,7 @@ mongoose.connect(process.env.MONGO_URL, {
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: {
-    origin: 'http://localhost:3000',
+    origin: '*',
     methods: ["GET", "POST"]
   }
 });
@@ -122,6 +122,6 @@ io.on("connection", (socket) => {
 
   });
 
-  httpServer.listen(3002, ()=> {
-    console.log("HTTP Server Running on port 3002")
+  httpServer.listen(PORT, ()=> {
+    console.log(`SOCKET_IO Server Running on port ${PORT}`)
   });
